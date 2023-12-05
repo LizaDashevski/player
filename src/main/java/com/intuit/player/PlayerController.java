551167333bc84525e8a3ec6715886d83769200ae
Eran Cohen <eranco@redhat.com>
2023-12-06 01:06:30 +0200

    use playerService in controller

--- src/main/java/com/intuit/player/PlayerController.java
+++ src/main/java/com/intuit/player/PlayerController.java
@@ -1,22 +1,24 @@
 package com.intuit.player;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/players")
 public class PlayerController {
+    @Autowired
+    private PlayerService playerService;
 
     /**
      * GET /api/players - returns the list of all players
      */
     @GetMapping()
-    List<Player> getAllPlayers() { return Collections.emptyList(); }
+    List<Player> getAllPlayers() { return playerService.getAll(); }
 
     /**
      * GET /api/players/{playerID} get single player by id
@@ -24,7 +26,5 @@ public class PlayerController {
      * @return   player as JSON
      */
     @GetMapping("/{id}")
-    Player getPlayer(@PathVariable String id) {
-        return null;
-    }
+    Player getPlayer(@PathVariable String id) { return playerService.get(id); }
 }
