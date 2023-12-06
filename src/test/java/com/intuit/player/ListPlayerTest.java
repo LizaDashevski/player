9daf51e0e73c74a31aa8eb463c7a185fed3c15cb
Eran Cohen <eranco@redhat.com>
2023-12-06 08:17:17 +0200

    add test case

--- src/test/java/com/intuit/player/ListPlayerTest.java
+++ src/test/java/com/intuit/player/ListPlayerTest.java
@@ -6,18 +6,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //random port
-public class listPlayerTest {
+public class ListPlayerTest {
 
     private static final String LOCALHOST = "http://localhost:";
     private static final String URI = "/api/players/";
 
     private static final String PLAYER_ID = "aardsda01";
-    private static final String FIRST_NAME = "David";
-    private static final String LAST_NAME = "Aardsma";
     private static final String GIVEN_NAME = "David Allan";
 
     @LocalServerPort
@@ -30,7 +29,7 @@ public class listPlayerTest {
      * Test get player with empty id
      */
     @Test
-    public void getWithNoId() {
+    public void getMissingId() {
         String url = LOCALHOST + port + URI;
         assertThat(restTemplate.getForEntity(url, Player.class).getStatusCode()== HttpStatus.NOT_FOUND);
 
@@ -39,9 +38,24 @@ public class listPlayerTest {
      * Test notfound ID for requesting player
      */
     @Test
-    public void badPlayerID() {
+    public void notFoundPlayerID() {
         String url = LOCALHOST + port + URI + "BAD_ID";
         assertThat(restTemplate.getForEntity(url, Player.class).getStatusCode() == HttpStatus.NOT_FOUND);
+    }
+
+
+    /**
+     * Test get player by ID success flow
+     */
+    @Test
+    public void getPlayerCorrectID()  {
+        String url = LOCALHOST + port + URI + PLAYER_ID;
+        ResponseEntity<Player> responseEntity = restTemplate.getForEntity(url, Player.class);
+        assertThat(responseEntity).isNotNull();
+        Player player = responseEntity.getBody();
+        if (player!=null) {
+            assertThat(GIVEN_NAME.equals(player.getNameGiven()));
+        }
     }
 
 }
